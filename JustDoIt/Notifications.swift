52f2075aa6cdc8eb42825413ec83c86d4376de3c
Ricardo Granados Macias <rickygranados2002@gmail.com>
2022-02-21 18:50:29 -0800

    Notifications

---
+++ JustDoIt/Notifications.swift
@@ -0,0 +1,16 @@
+//
+//  Notifications.swift
+//  JustDoIt
+//
+//  Created by csuftitan on 2/21/22.
+//
+
+//push to notification branch
+import Foundation
+
+
+struct Notification {
+    var message = ""
+    var ListOfMessages = ["Oh "]
+    
+}
