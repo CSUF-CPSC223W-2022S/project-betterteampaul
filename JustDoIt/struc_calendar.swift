a0ad59d6fad5fb37732e3dfaa5d982d2382bf7c1
Alexander Zavaleta <A_zavaleta@csu.fullerton.edu>
2022-02-21 17:58:47 -0800

    Project

---
+++ JustDoIt/struc_calendar.swift
@@ -0,0 +1,15 @@
+//
+//  struc_calendar.swift
+//  JustDoIt
+//
+//  Created by Alexander Zavaleta on 2/21/22.
+//
+
+import Foundation
+
+struct calendar{
+    var day: String
+    var week: String
+    var month: String
+    var year: String
+}
