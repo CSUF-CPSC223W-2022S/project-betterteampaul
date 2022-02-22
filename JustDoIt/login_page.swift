5ad8fbc0c8fc07d9108487f6fbd4eed2e8bfd355
AlexSpandex <78000095+AlexSpandex@users.noreply.github.com>
2022-02-21 18:17:28 -0800

    Login page structure

---
+++ JustDoIt/login_page.swift
@@ -0,0 +1,13 @@
+//
+//  login_page.swift
+//  JustDoIt
+//
+//  Created by Alejandro Ramos on 2/21/22.
+//
+
+import Foundation
+
+struct LoginPage {
+    var email: String;
+    var password: String;
+}
