53ebda5acf1151e8fb074e8c90a429c88be4afb6
ricardogrm02 <70675644+ricardogrm02@users.noreply.github.com>
2022-04-05 21:57:18 -0700

    Update JustDoItTests.swift

--- JustDoItTests/JustDoItTests.swift
+++ JustDoItTests/JustDoItTests.swift
@@ -1,3 +1,4 @@
+//
 //  JustDoItTests.swift
 //  JustDoItTests
 //
@@ -7,15 +8,12 @@ import XCTest
 @testable import JustDoIt
 
 class JustDoItTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -23,12 +21,10 @@ class JustDoItTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
