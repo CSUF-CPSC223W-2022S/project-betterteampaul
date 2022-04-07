94438a89da3fefd621979d905935f6a13070178e
AlexSpandex <78000095+AlexSpandex@users.noreply.github.com>
2022-04-06 21:07:46 -0700

    Added Sign up Page

--- JustDoIt/ViewController.swift
+++ JustDoIt/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 var testEmail: String = "studentemail@csu.fullerton.edu"
 var testPassword: String = "student"
 var testCheck: String = "student"
@@ -15,7 +16,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var usernameTextField: UITextField!
     
-   
+  
    // var loginObj = LoginPage(email: testEmail, password: testPassword)
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
